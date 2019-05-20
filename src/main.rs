70c911a88a0b6db909b58b254413d3ac337242bb
emergent <yosikawa@gmail.com>
2019-05-20 19:26:53 +0900

    trying to implement callback between thread (but cannot compile)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 extern crate rust_mimiio;
 use clap::{App, Arg};
 use rust_mimiio::MimiIO;
+use std::cell::RefCell;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
+use std::sync::Arc;
+
+const CHUNK_SIZE: usize = 8192;
 
 fn read_token(filename: &str) -> Result<String, String> {
     let mut s = String::new();
@@ -34,7 +38,7 @@ fn run() -> Result<(), String> {
         )
         .get_matches();
 
-    let _input_file = matches.value_of("input").unwrap();
+    let input_file = matches.value_of("input").unwrap();
 
     let token_filename = matches.value_of("token").unwrap();
     let token = read_token(token_filename)?;
@@ -46,7 +50,29 @@ fn run() -> Result<(), String> {
     headers.insert("Content-Type", "audio/x-pcm;bit=16;rate=16000;channels=1");
     headers.insert("Authorization", &token_format);
 
-    let mio = MimiIO::open("service.mimi.fd.ai", 443, &headers)?;
+    let mut f = File::open(input_file).expect("file open error");
+    let mut buf = [0u8; CHUNK_SIZE];
+    let mut total = 0;
+
+    let mio = MimiIO::open(
+        "service.mimi.fd.ai",
+        443,
+        &headers,
+        Arc::new(move |send_buf: &mut Vec<u8>, recog_break: &mut bool| {
+            let size = f.read(&mut buf).unwrap();
+            match size {
+                0 => *recog_break = true,
+                _ => {
+                    for &d in buf.iter() {
+                        send_buf.push(d);
+                    }
+                }
+            }
+        }),
+        &mut Arc::new(move |recv_str, _finished| {
+            println!("{}", recv_str);
+        }),
+    )?;
 
     mio.close();
     Ok(())
